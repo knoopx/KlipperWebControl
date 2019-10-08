121d7bdb60f1b7f4dcdc4129fcb975007ab6434f
Victor Martinez <knoopx@gmail.com>
2019-10-09 00:14:56 +0200

    Remove uneeded webpack plugins

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,15 @@
-const CompressionPlugin = require('compression-webpack-plugin')
-const ZipPlugin = require('zip-webpack-plugin')
+const ZipPlugin = require("zip-webpack-plugin");
 
 module.exports = {
-	configureWebpack: {
-		performance: { hints: false },
-		plugins: (process.env.NODE_ENV === 'production') ? [
-			new CompressionPlugin({
-				cache: true
-			}),
-			new ZipPlugin({
-				filename: 'DuetWebControl-SD.zip',
-				include: [/\.gz$/, /\.woff$/, /\.woff2$/],
-			}),
-			new ZipPlugin({
-				filename: 'DuetWebControl-SBC.zip',
-				exclude: [/\.gz$/, /\.zip$/]
-			})
-		] : []
-	},
-	chainWebpack: config => {
-		config.optimization.delete('splitChunks')
-	}
-}
+  configureWebpack: {
+    performance: { hints: false },
+    plugins: [
+      new ZipPlugin({
+        filename: "KlipperWebControl.zip",
+      })
+    ]
+  },
+  chainWebpack: config => {
+    config.optimization.delete("splitChunks");
+  }
+};
