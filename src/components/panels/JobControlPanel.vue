45f4940451e399660b8b524c21767c25fed3ae39
Victor Martinez <knoopx@gmail.com>
2019-10-18 11:45:00 +0000

    PAUSE/RESUME

--- src/components/panels/JobControlPanel.vue
+++ src/components/panels/JobControlPanel.vue
@@ -5,11 +5,11 @@
 		</v-card-title>
 
 		<v-card-text class="pt-0">
-			<code-btn color="warning" block :disabled="uiFrozen || !isPrinting" :code="isPaused ? 'M24' : 'M25'" tabindex="0">
+			<code-btn color="warning" block :disabled="uiFrozen || !isPrinting" :code="isPaused ? 'RESUME' : 'PAUSE'" tabindex="0">
 				<v-icon class="mr-1">{{ isPaused ? "play_arrow" : "pause" }}</v-icon> {{ pauseResumeText }}
 			</code-btn>
 
-			<code-btn v-if="isPaused" color="error" block code="M0 H1">
+			<code-btn v-if="isPaused" color="error" block code="ABORT">
 				<v-icon class="mr-1">stop</v-icon> {{ cancelText }}
 			</code-btn>
 
